36acbfcae4397c2c165cd9137c9ffcdbf34eac65
isbankov@hotmail.com <isbankov@hotmail.com>
2022-10-05 22:43:16 +0100

    15. Implementing Linked List -------------------------------- SoftUni.bg C# Advanced - септември 2022 15. Implementing Linked List All tasks completed - 100%

---
+++ 02 C# Advanced/15 Implementing Linked List/15 Custom Doubly Linked List/Doubly_Linked_List/DoublyLinkedList.cs
@@ -0,0 +1,127 @@
+﻿namespace CustomDoublyLinkedList
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    internal class DoublyLinkedList
+    {
+        private ListNode head;
+        private ListNode tail;
+
+        public int Count { get; private set; }
+
+        public DoublyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+            this.Count = 0;
+        }
+
+        public void AddFirst(int element)
+        {
+            if(this.Count == 0)
+            {
+                this.head = this.tail = new ListNode(element);
+            }
+            else
+            {
+                ListNode newHead = new ListNode(element);
+                newHead.NextNode = this.head;
+                this.head.PreviousNode = newHead;
+                this.head = newHead;
+            }
+
+            this.Count++;
+
+        }
+
+        public void AddLast(int element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode(element);
+            }
+            else
+            {
+                ListNode newTail = new ListNode(element);
+                newTail.PreviousNode = this.tail;
+                this.tail.NextNode = newTail;
+                this.tail = newTail;
+            }
+
+            this.Count++;
+        }
+
+        public int RemoveFirst()
+        {
+            if(this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            var ret = this.head.Value;
+            this.head = this.head.NextNode;
+            if(this.Count == 1)
+            {
+                this.tail = null;
+            }
+            else
+            {
+                this.head.PreviousNode = null;
+            }
+
+            this.Count--;
+            return ret;
+        }
+
+        public int RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            var ret = this.tail.Value;
+            this.tail = this.tail.PreviousNode;
+            if (this.Count == 1)
+            {
+                this.head = null;
+            }
+            else
+            {
+                this.tail.NextNode = null;
+            }
+
+            this.Count--;
+            return ret;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            var currentNode = this.head;
+            while(currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            var ret = new int[this.Count];
+            int counter = 0;
+            var currentNode = this.head;
+
+            while (currentNode != null)
+            {
+                ret[counter] = currentNode.Value;
+                currentNode = currentNode.NextNode;
+                counter++;
+            }
+
+            return ret;
+        } 
+
+    }
+}
---
+++ 02 C# Advanced/15 Implementing Linked List/15 Custom Doubly Linked List/Doubly_Linked_List/ListNode.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomDoublyLinkedList
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    internal class ListNode
+    {
+        public int Value { get; set; }
+        public ListNode NextNode { get; set; }
+        public ListNode PreviousNode { get; set; }
+
+        public ListNode(int value) { this.Value = value; }
+
+    }
+}
