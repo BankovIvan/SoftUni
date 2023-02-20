2f3ee7aa20c022c852881a51dd3b68a8952e081d
isbankov@hotmail.com <isbankov@hotmail.com>
2023-02-20 22:48:28 +0000

    4. Exercise: ADO.NET ----------------------- SoftUni.bg Entity Framework Core - февруари 2023 All tasks completed.

--- 05 Entity Fr. Core/04 Ex ADO.NET/Ex ADO.NET/2. Villain Names/SqlQueries.cs
+++ 05 Entity Fr. Core/04 Ex ADO.NET/Ex ADO.NET/2. Villain Names/SqlQueries.cs
@@ -69,9 +69,52 @@
         public const string _04_InsertIntoTowns =
             @"INSERT INTO Towns(Name) VALUES(@townName)"; //
 
+        public const string _05_UpdateTownsByCountry =
+            @"UPDATE 
+                Towns
+            SET 
+                Name = UPPER(Name)
+            WHERE 
+                CountryCode = 
+                    (SELECT c.Id FROM Countries AS c WHERE c.Name = @countryName)";
 
+        public const string _05_SelectTownsByCountry =
+            @"SELECT 
+                t.Name 
+            FROM 
+                Towns as t
+                JOIN Countries AS c 
+                ON c.Id = t.CountryCode
+            WHERE 
+                c.Name = @countryName";
 
-        
+        public const string _06_SelectVillainName =
+            @"SELECT Name FROM Villains WHERE Id = @villainId";
+
+        public const string _06_DeleteVillainRelation=
+            @"DELETE FROM MinionsVillains WHERE VillainId = @villainId";
+
+        public const string _06_DeleteVillain =
+            @"DELETE FROM Villains WHERE Id = @villainId";
+
+        public const string _07_SelectMinionNames =
+            @"SELECT Name FROM Minions";
+
+        public const string _08_UpdateMinionNamesAndAges =
+            @"UPDATE
+                Minions
+            SET 
+                Name = LOWER(LEFT(Name, 1)) + SUBSTRING(Name, 2, LEN(Name))
+                , Age += 1
+            WHERE 
+                Id = @Id";
+
+        public const string _08_SelectMinionNamesAndAges =
+            @"SELECT Name, Age FROM Minions";
+
+        public const string _09_MinionsAgeStoredProcedure =
+            @"usp_GetOlder";
+
 
     }
 }
--- 05 Entity Fr. Core/04 Ex ADO.NET/Ex ADO.NET/2. Villain Names/StartUp.cs
+++ 05 Entity Fr. Core/04 Ex ADO.NET/Ex ADO.NET/2. Villain Names/StartUp.cs
@@ -3,6 +3,7 @@
     using Microsoft.Data.SqlClient;
     using System.Data;
     using System.Reflection.PortableExecutable;
+    using System.Security.Cryptography.X509Certificates;
     using System.Text;
     using System.Threading.Tasks;
 
@@ -41,14 +42,68 @@
             */
 
             //4. Add Minion
+            /*
             Console.WriteLine("4. Add Minion");
             Console.WriteLine("----------");
-
             string[] arrMinion = Console.ReadLine().Split(new char[] { ':', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
             string[] arrVillain = Console.ReadLine().Split(new char[] { ':', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
             string ret04 = await _04_AddMinionAsync(connection, arrMinion, arrVillain);
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine(ret04);
+            Console.ResetColor();
+            */
+
+            //5. Change Town Names Casing
+            /*
+            Console.WriteLine("5. Change Town Names Casing");
+            Console.WriteLine("----------");
+            string sTownName = Console.ReadLine();
+            string ret05 = await _05_ChangeTownNamesAsync(connection, sTownName);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(ret05);
+            Console.ResetColor();
+            */
+
+            //6. *Remove Villain 
+            /*
+            Console.WriteLine("6. *Remove Villain ");
+            Console.WriteLine("----------");
+            int nVilanId = int.Parse(Console.ReadLine());
+            string ret06 = await _06_DeleteVillainByIdAsync(connection, nVilanId);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(ret06);
+            Console.ResetColor();
+            */
+
+            //7. Print All Minion Names 
+            /*
+            Console.WriteLine("7. Print All Minion Names");
+            Console.WriteLine("----------");
+            string ret06 = await _07_PrintMinionNamesAsync(connection);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(ret06);
+            Console.ResetColor();
+            */
+
+            //8. Increase Minion Age
+            /*
+            Console.WriteLine("8. Increase Minion Age");
+            Console.WriteLine("----------");
+            int[] arrMinion = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            string ret08 = await _08_IncreaseMinionAgeAsync(connection, arrMinion);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(ret08);
+            Console.ResetColor();
+            */
+
+            //9. Increase Age Stored Procedure 
+            //
+            Console.WriteLine("9. Increase Age Stored Procedure ");
+            Console.WriteLine("----------");
+            int nId = int.Parse(Console.ReadLine());
+            string ret09 = await _09_AgeStoredProcedureAsync(connection, nId);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(ret09);
             Console.ResetColor();
 
             connection.Close();
@@ -233,5 +288,241 @@
             return ret.ToString().Trim();
 
         }
+
+
+        /// <summary>
+        ///     5. Change Town Names Casing
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <returns></returns>
+        private static async Task<string> _05_ChangeTownNamesAsync(SqlConnection connection, string sTownName)
+        {
+            using SqlCommand commandUpdateTowns = new SqlCommand(SqlQueries._05_UpdateTownsByCountry, connection);
+
+            commandUpdateTowns.Parameters.Add(new SqlParameter("@countryName", sTownName));
+
+            int nRows = await commandUpdateTowns.ExecuteNonQueryAsync();
+
+            StringBuilder ret = new StringBuilder();
+
+            if(nRows > 0)
+            {
+                ret.AppendLine(String.Format("{0} town names were affected.", nRows));
+            }
+            else
+            {
+                ret.AppendLine("No town names were affected.");
+                return ret.ToString().Trim();
+            }
+
+            using SqlCommand commandSelectTowns = new SqlCommand(SqlQueries._05_SelectTownsByCountry, connection);
+            commandSelectTowns.Parameters.Add(new SqlParameter("@countryName", sTownName));
+
+            using SqlDataReader readerTowns = await commandSelectTowns.ExecuteReaderAsync();
+
+            List<string> towns = new List<string>();
+
+            while (readerTowns.Read())
+            {
+                towns.Add((string)readerTowns["Name"]);
+
+            }
+
+            ret.AppendLine("[" + String.Join(", ", towns) + "]");
+
+            readerTowns.Close();
+
+            return ret.ToString().Trim();
+
+        }
+
+
+        /// <summary>
+        ///     6. *Remove Villain 
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <returns></returns>
+        private static async Task<string> _06_DeleteVillainByIdAsync(SqlConnection connection, int nVillainId)
+        {
+            using SqlCommand commandVillain = new SqlCommand(SqlQueries._06_SelectVillainName, connection);
+
+            commandVillain.Parameters.Add(new SqlParameter("@villainId", nVillainId));
+
+            string? sVilanName = (string?)await commandVillain.ExecuteScalarAsync();
+
+            StringBuilder ret = new StringBuilder();
+
+            if (sVilanName == null)
+            {
+                ret.AppendLine("No such villain was found.");
+                return ret.ToString().Trim();
+            }
+
+            //BEGIN TRANSACTION:
+            SqlTransaction transaction = connection.BeginTransaction();
+
+            try
+            {
+                // Check the town:
+                SqlCommand commandDeleteVillainRelations = connection.CreateCommand();
+                commandDeleteVillainRelations.Connection = connection;
+                commandDeleteVillainRelations.Transaction = transaction;
+                commandDeleteVillainRelations.CommandText = SqlQueries._06_DeleteVillainRelation;
+                commandDeleteVillainRelations.Parameters.AddWithValue("@villainId", nVillainId);
+
+                int nRelationsDeleted = await commandDeleteVillainRelations.ExecuteNonQueryAsync();
+
+                // Check the villain:
+                SqlCommand commandDeleteVillain = connection.CreateCommand();
+                commandDeleteVillain.Connection = connection;
+                commandDeleteVillain.Transaction = transaction;
+                commandDeleteVillain.CommandText = SqlQueries._06_DeleteVillain;
+                commandDeleteVillain.Parameters.AddWithValue("@villainId", nVillainId);
+
+                int nVillainDeleted = await commandDeleteVillain.ExecuteNonQueryAsync();
+
+                ret.AppendLine(String.Format("{0} was deleted.", sVilanName));
+                ret.AppendLine(String.Format("{0} minions were released.", nRelationsDeleted));
+
+                await transaction.CommitAsync();
+
+            }
+            catch (Exception e)
+            {
+                await transaction.RollbackAsync();
+
+            }
+
+            return ret.ToString().Trim();
+
+        }
+
+
+        /// <summary>
+        ///     7. Print All Minion Names
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <returns></returns>
+        private static async Task<string> _07_PrintMinionNamesAsync(SqlConnection connection)
+        {
+            using SqlCommand commandMinions = new SqlCommand(SqlQueries._07_SelectMinionNames, connection);
+
+            StringBuilder ret = new StringBuilder();
+
+            using SqlDataReader readerMinions = await commandMinions.ExecuteReaderAsync();
+
+            List<string> towns = new List<string>();
+
+            while (readerMinions.Read())
+            {
+                towns.Add((string)readerMinions["Name"]);
+            }
+
+            while(towns.Count> 0)
+            {
+                ret.AppendLine(towns[0]);
+                towns.RemoveAt(0);
+
+                if(towns.Count > 0)
+                {
+                    ret.AppendLine(towns[towns.Count - 1]);
+                    towns.RemoveAt(towns.Count - 1);
+                }
+            }
+
+            return ret.ToString().Trim();
+
+        }
+
+        /// <summary>
+        ///     8. Increase Minion Age
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <returns></returns>
+        private static async Task<string> _08_IncreaseMinionAgeAsync(SqlConnection connection, int[] arrMinionsId)
+        {
+            //BEGIN TRANSACTION:
+            SqlTransaction transaction = connection.BeginTransaction();
+
+            try
+            {
+                for(int i = 0; i < arrMinionsId.Length; i++)
+                {
+                    using SqlCommand commandUpdateMinions = connection.CreateCommand();
+                    commandUpdateMinions.Connection = connection;
+                    commandUpdateMinions.Transaction = transaction;
+                    commandUpdateMinions.CommandText = SqlQueries._08_UpdateMinionNamesAndAges;
+                    commandUpdateMinions.Parameters.AddWithValue("@Id", arrMinionsId[i]);
+
+                    int nMinionsUpdated = await commandUpdateMinions.ExecuteNonQueryAsync();
+                }
+
+                await transaction.CommitAsync();
+
+            }
+            catch (Exception e)
+            {
+                await transaction.RollbackAsync();
+
+            }
+
+            using SqlCommand commandMinions = new SqlCommand(SqlQueries._08_SelectMinionNamesAndAges, connection);
+
+            StringBuilder ret = new StringBuilder();
+
+            using SqlDataReader readerMinions = await commandMinions.ExecuteReaderAsync();
+
+            while (readerMinions.Read())
+            {
+                ret.AppendLine(String.Format("{0} {1}", (string)readerMinions["Name"], (int)readerMinions["Age"]));
+            }
+
+            return ret.ToString().Trim();
+
+        }
+
+        /// <summary>
+        ///     9. Increase Age Stored Procedure 
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <returns></returns>
+        private static async Task<string> _09_AgeStoredProcedureAsync(SqlConnection connection, int nMinionId)
+        {
+            //BEGIN TRANSACTION:
+            SqlTransaction transaction = connection.BeginTransaction();
+
+            StringBuilder ret = new StringBuilder();
+
+            try
+            {
+                using SqlCommand commandUpdateMinions = connection.CreateCommand();
+                commandUpdateMinions.CommandType = CommandType.StoredProcedure;
+                commandUpdateMinions.Connection = connection;
+                commandUpdateMinions.Transaction = transaction;
+                commandUpdateMinions.CommandText = SqlQueries._09_MinionsAgeStoredProcedure;
+                commandUpdateMinions.Parameters.AddWithValue("@id", nMinionId);
+
+                using SqlDataReader readerMinions = await commandUpdateMinions.ExecuteReaderAsync();
+
+                while (readerMinions.Read())
+                {
+                    ret.AppendLine(String.Format("{0} – {1} years old", (string)readerMinions["Name"], (int)readerMinions["Age"]));
+                }
+
+                readerMinions.Close();
+
+                await transaction.CommitAsync();
+
+            }
+            catch (Exception e)
+            {
+                await transaction.RollbackAsync();
+
+            }
+
+            return ret.ToString().Trim();
+
+        }
+
     }
 }
